04d44881df4c4e87efae1c9517825d40eac2b416
Keith Clawson <kclawson@wsu.edu>
2013-02-18 09:59:39 -0800

    Improved performance.
    Forced bash to ignore .bashrc, so prompt always
    defaults to "bash-4.2$ ".
    
    Removed prompt detection and fixed prompt option.
    
    Branched based on beep character. Bash always beeps
    when it receives two tabs, so we avoid ever timing
    out the expect call.

--- shell_completions.py
+++ shell_completions.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 
 # Known bugs/oddities:
-#   [ ] Fails to correctly detect the prompt with certain .bashrc settings.
+#   [X] Fails to correctly detect the prompt with certain .bashrc settings.
 #   [X] Spurious empty strings/escape sequences appear with multi-page output.
 #   [ ] Output is not consistent. Example: 'git stat' returns 'git status', while
 #       'git sta' returns a list of completions only for the 'sta' part of the 
@@ -10,10 +10,14 @@
 import argparse, pexpect, sys
 
 COMPLETIONS_COMMAND = ". /etc/bash_completion"
+COMPLETIONS_COMMAND_TIMEOUT = 1
 BIGLIST_WARNING = "(y or n)"
 NEXT_PAGE_INDICATOR = "--More--"
-DEFAULT_TIMEOUT=2
-DEFAULT_SHELL="bash"
+DEFAULT_TIMEOUT=0.1
+DEFAULT_SHELL="bash --norc"
+DEFAULT_PROMPT="bash-4.2$ "
+BEEP="\x07"
+NEWLINE="\r\n"
 
 
 def print_string(message, string):
@@ -21,119 +25,106 @@ def print_string(message, string):
 
 
 def completions(partial_command, shell=DEFAULT_SHELL, return_raw=False, 
-                import_completions=True, get_prompt=True, prompt="$ ",
+                import_completions=True, set_prompt=False, prompt=None,
                 timeout=DEFAULT_TIMEOUT, biglist=True, verbose=False,
                 logfile=None):
     """
     Returns a list containing the tab completions found by the shell for the 
     input string.
     """
+    if prompt is None:
+        prompt = DEFAULT_PROMPT
 
     child = pexpect.spawn(shell, timeout=timeout)
-
-    if verbose:
-        child.logfile = sys.stdout
+    child.expect_exact(prompt)
 
     if logfile is not None:
         logfile = open(logfile, "w")
         child.logfile = logfile 
 
-    # We want to echo characters sent to the shell so that new prompts print
-    # on their own line.
-    child.setecho(True)
-
-    # echo_on = child.getecho()
-    # if verbose:
-    #    print "Echo state: " + str(echo_on)
-
-    # Get a bare command prompt in order to find the end of the
-    # list of completions.
-    if get_prompt:
-
-        # !!!
-        # Here we assume that the shell will only print out a command
-        # prompt on startup. This is not always true.
-        # !!!
-        child.sendline()
-        child.expect_exact("\r\n")
-        prompt = child.before
-
-        # We just hit enter, so we expect the shell to print a new prompt and
-        # we need to clear it out of the buffer.
-        child.expect_exact(prompt)
-
-        if verbose:
-            print_string("Prompt:", prompt)
+    if verbose:
+        print_string("Python version:", sys.version)
+        print_string("Echo state:", child.getecho())
+        # child.logfile = sys.stdout
 
     # Run a script to configure extra bash completions.
     if import_completions:
         child.sendline(COMPLETIONS_COMMAND)
-        child.expect_exact(prompt)
-
+        child.expect_exact(prompt, timeout=COMPLETIONS_COMMAND_TIMEOUT)
+        
     child.send(partial_command + "\t\t")
-    child.expect_exact(partial_command)
-    #### NOTE: I don't understand why this time we don't get an echo.
-    ####       New idea: Of course... it's only echoing the sent characters.
-    # child.expect_exact(partial_command)
 
-    index = child.expect_exact([" ", "\r\n", pexpect.TIMEOUT])
-
+    index = child.expect_exact([BEEP, pexpect.TIMEOUT])
+ 
     if index == 0:
-        # Bash found a single completion and filled it in.
-        return [partial_command + child.before]
+        # Shell beeped at us. It will always beep at least once when it receives the second
+        # tab character.
+
+        if verbose:
+            print "BEFORE: " + child.before
+            print "PARTIAL: " + partial_command
+
+        # Shell beeped before printing any completions.
+        if child.before == partial_command:
+            
+            index = child.expect_exact([BEEP, NEWLINE])
+            
+            if index == 0:
+                # The completion fit on one line.
+                return [partial_command + child.before]
+            
+            elif index == 1:
+                # The completion required multiple lines.
+
+                index = child.expect_exact([BIGLIST_WARNING, NEXT_PAGE_INDICATOR, prompt])
+                if index == 0 or index == 1:
+                    # The shell found so many completions that we must page through them.
+                    if biglist:
+                        completions = ""
+
+                        # For very long lists the shell asks whether to continue.
+                        if index == 0:
+                            child.send("y")
+                        
+                        # Shorter lists print to the screen without asking. 
+                        else:
+                            completions += child.before
+                            child.send(" ")
+                        
+                        # Keep sending space to get more pages until we get back to the 
+                        # command prompt.
+                        while True:
+                            index = child.expect_exact([NEXT_PAGE_INDICATOR, prompt])
+                            completions += child.before
+                            if index == 0:
+                                child.send(" ")
+                            elif index == 1:
+                                break
+                        
+                        # Remove spurious escape sequence.
+                        completions = completions.replace("\x1b[K", "").split()
+
+                elif index == 2:
+                    # The completions required multiple lines, but still fit on one page.
+                    completions = child.before.split()
+
+        else:
+            # Shell beeped at the end of the completion. This happens when a single completion
+            # is found and the shell automatically fills it in.
+            completions = [child.before]
 
     elif index == 1:
-        index = child.expect_exact([BIGLIST_WARNING, NEXT_PAGE_INDICATOR, prompt])
-        if index == 0 or index == 1:
-            # The shell found too many completions to list on one screen.
-            if biglist:
-                completions = ""
+        # Timed out. 
+        # Either there was no completion found, or it took too long to search.
+        completions = [""]
 
-                # For very long lists the shell asks whether to continue.
-                if index == 0:
-                    child.send("y")
-                
-                # Shorter lists print to the screen without asking. 
-                else:
-                    completions += child.before
-                    child.send(" ")
-                
-                # Keep sending space to get more pages until we get back to the 
-                # command prompt.
-                while True:
-                    index = child.expect_exact([NEXT_PAGE_INDICATOR, prompt])
-                    completions += child.before
-                    if index == 0:
-                        child.send(" ")
-                    elif index == 1:
-                        break
-                
-                # Remove spurious escape sequence.
-                completions = completions.replace("\x1b[K", "")
-
-        elif index == 2:
-            # Bash found more than one completion and listed them on multiple lines.
-            # child.expect_exact(prompt)
-            completions = child.before
- 
-    elif index == 2:
-        # If the command timed out, either no completion was found or it
-        # found a single completion witout adding a space (for instance, this 
-        # happens when completing the name of an executable).
-
-        # print_string("Timed out:", child.before)
-
-        # Remove any bell characters the shell appended to the completion.
-        return [partial_command + child.buffer.replace("\x07", "")]
-    
     child.close()
 
-    # Parse the completions into a Python list of strings.
-    return completions.split()
+    return completions 
 
 
 if __name__ == "__main__":
-    
+
     parser = argparse.ArgumentParser(
         description="Returns the tab completions found by the shell for the input string.")
 
@@ -143,7 +134,7 @@ if __name__ == "__main__":
     parser.add_argument("--no_biglists", action="store_false", default=True,
                         help="Abort execution if the shell finds a large number of completions.")
 
-    parser.add_argument("--no_detect_prompt", action="store_false", default=True,
+    parser.add_argument("--prompt", default=None,
                         help="Don't attempt to detect the command prompt, and use a built-in constant instead. This should speed up execution times.")
 
     parser.add_argument("--no_import_completions", default=True,
@@ -169,7 +160,7 @@ if __name__ == "__main__":
     args = parser.parse_args()
 
     completion_list = completions(args.COMMAND, verbose=args.verbose, 
-        return_raw=args.raw, get_prompt=args.no_detect_prompt, 
-        timeout=args.timeout, biglist=args.no_biglists, logfile=args.log)
+        return_raw=args.raw, timeout=args.timeout, biglist=args.no_biglists, 
+        prompt=args.prompt, logfile=args.log)
 
     print str(args.separator).join(completion_list)
